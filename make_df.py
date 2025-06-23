b660ca08b55524f3dc90889b91d34abc8db60ab9
therealpeppy <furghadmanar@gmail.com>
2025-06-22 22:43:45 -0500

    make multiple subplots for different 'grouping'

--- make_df.py
+++ make_df.py
@@ -2,20 +2,28 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-some_ints = np.arange(0, 1000)
-df = pd.DataFrame(data=some_ints, columns=['some_ints'])
-df['some_dates'] = pd.to_datetime('2025-06-02') + pd.to_timedelta(df.some_ints/1000, unit='D')
+generate = False
 
-df.set_index('some_dates', inplace=True)
+if generate:
+    some_ints = np.arange(0, 1000)
+    df = pd.DataFrame(data=some_ints, columns=['some_ints'])
+    df['some_dates'] = pd.to_datetime('2025-06-02') + pd.to_timedelta(df.some_ints/1000, unit='D')
 
-df['cosine'] = np.cos(df.some_ints/100 * np.pi)
-df['sine'] = np.sin(df.some_ints/100  * np.pi)
-df['steps'] = (((df.some_ints/10).round(0) % 10)==0).astype(int)
+    df.set_index('some_dates', inplace=True)
 
-print(df)
+    df['cosine'] = np.cos(df.some_ints/100 * np.pi)
+    df['sine'] = np.sin(df.some_ints/100  * np.pi)
+    df['steps'] = (((df.some_ints/10).round(0) % 10)==0).astype(int)
+else:
+    df = pd.read_csv('example_dataframe.csv')
 
-plt.plot(df.index, df.cosine, linewidth=0.5)
-plt.plot(df.index, df.sine, linewidth=0.5)
-plt.plot(df.index, df.steps, linewidth=0.5)
+print("Succsesfully loaded/generated DataFrame")
+print(df.head())
+
+fig, axs = plt.subplots(3)
+axs[0].plot(df.index, df.cosine, linewidth=0.5)
+axs[1].plot(df.index, df.sine, linewidth=0.5)
+axs[2].plot(df.index, df.steps, linewidth=0.5)
+
 plt.show()
 
