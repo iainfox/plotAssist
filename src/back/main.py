350a70cf95656a6f00b8e8d9d1b7c10f752ed81b
iainfox <iainpfox@gmail.com>
2026-06-10 23:26:44 -0500

    create basic backend startup script

--- src/back/main.py
+++ src/back/main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import webview
+from pathlib import Path
+
+DEV = True
+
+if DEV:
+    url = "http://localhost:5173"
+else: 
+    frontend = (
+            Path(__file__).resolve().parent.parent
+            / "front"
+            / "dist"
+            / "index.html"
+            )
+
+    if not frontend.exists():
+        raise FileNotFoundError(f"frontend build not found: {frontend}")
+
+    url = frontend.as_uri()
+
+webview.create_window("App", url, width=1050, height=400)
+webview.start(gui='edgechromium', debug=True)
