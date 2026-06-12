f0ce31ba9e9c9e9da1be156182d546b09e2092ef
iainfox <iainpfox@gmail.com>
2026-06-11 21:00:53 -0500

    move window start to a function to allow 3rd party scripts to import and start it

--- src/back/main.py
+++ src/back/main.py
@@ -1,22 +1,23 @@
 import webview
-from pathlib import Path
+import pandas as pd
 
 DEV = True
 
-if DEV:
-    url = "http://localhost:5173"
-else: 
-    frontend = (
-            Path(__file__).resolve().parent.parent
-            / "front"
-            / "dist"
-            / "index.html"
-            )
+url = "http://localhost:5173"
 
-    if not frontend.exists():
-        raise FileNotFoundError(f"frontend build not found: {frontend}")
+class Api:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
 
-    url = frontend.as_uri()
+    def get_data(self):
+        data = self.data.to_json()
+        if (not data):
+            raise Exception("Couldn't convert data to json")
 
-webview.create_window("App", url, width=1081, height=439)
-webview.start(gui='edgechromium', debug=True)
+        return data
+
+def start(data: pd.DataFrame):
+    api = Api(data)
+
+    webview.create_window("App", url, width=1081, height=439, js_api=api)
+    webview.start(gui='edgechromium', debug=True)
