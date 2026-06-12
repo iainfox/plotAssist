8063e9133efec95e307001428061b1e7a9cf5de4
iainfox <iainpfox@gmail.com>
2026-06-11 22:21:47 -0500

    create function to add all channel names to the available channels list

--- src/front/src/main.ts
+++ src/front/src/main.ts
@@ -1,7 +1,20 @@
+import {available_channels_list} from './dom'
 import './style.css'
+import type {AppData} from './types'
 
 window.addEventListener('pywebviewready', async () => {
 	const data = await window.pywebview.api.get_data()
 
-	console.log(data)
+	addChannels(data)
 })
+
+function addChannels(data: AppData) {
+	const channel_names = data.channels.map((channel) => channel.name)
+
+	channel_names.forEach((channel_name) => {
+		const li = document.createElement("li")
+		li.innerHTML = `<p>${channel_name}</p>`
+
+		available_channels_list.appendChild(li)
+	})
+}
